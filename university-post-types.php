b55b7c38e20e62b524e8d93c2afe27ccb23c2fdb
joynob <joynob@lexaeon.com>
2023-11-13 05:45:52 -0500

    adding upto section 8

--- university-post-types.php
+++ university-post-types.php
@@ -1,12 +1,15 @@
 <?php 
 function university_post_types(){
-
+// this is event post type
     register_post_type("event", array(
-
+        "supports" => array("title", "editor", "excerpt"),
+        "rewrite" => array("slug" => "events"),
+        "has_archive" => true,
         "public" => true,
+        "show_in_rest" => true,
         "labels" => array(
             "name" => "Events",
-            "add_new_item" => "Add New Event",
+            "add_new" => "Add New Event",
             "edit_item" => "Edit Event",
             "all_items" => "All Events",
             "singular_name" => "Event"
@@ -15,7 +18,23 @@ function university_post_types(){
 
     ));
 
-}
+// this is programme post type
+register_post_type("program", array(
+    "supports" => array("title", "editor"),
+    "rewrite" => array("slug" => "programs"),
+    "has_archive" => true,
+    "public" => true,
+    'show_in_rest' => true,
+    "labels" => array(
+        "name" => "programs",
+        "add_new" => "Add New program",
+        "edit_item" => "Edit program",
+        "all_items" => "All programs",
+        "singular_name" => "program"
+    ),
+    "menu_icon" => "dashicons-awards"
+));
 
+}
 add_action("init", "university_post_types");
 ?>
